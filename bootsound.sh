0a633ceadf861743e634f8a5a62e32bf40fed599
lithid <long.jeremie@gmail.com>
2011-06-23 09:44:08 -0400

    Added if statement
    This will make sure that anybody who runs this will not break their system if they run this a 2nd time.

--- bootsound.sh
+++ bootsound.sh
@@ -13,41 +13,47 @@ else
     recoverymode=n
 fi
 
-#This is to create the needed scripts in init.local.rc
-echo '' >> /system/etc/init.local.rc
-echo '' >> /system/etc/init.local.rc
-echo '###Bootsound - Safe to Delete: Start ###' >> /system/etc/init.local.rc
-echo "" >> /system/etc/init.local.rc
-echo 'service bootsound /system/bin/bootsound' >> /system/etc/init.local.rc
-echo '        user media' >> /system/etc/init.local.rc
-echo '        group audio' >> /system/etc/init.local.rc
-echo '        disabled' >> /system/etc/init.local.rc
-echo '        oneshot' >> /system/etc/init.local.rc
-echo '' >> /system/etc/init.local.rc
-echo 'on property:init.svc.bootanim=running   # to correct timin' >> /system/etc/init.local.rc
-echo '        start bootsound' >> /system/etc/init.local.rc
-echo '' >> /system/etc/init.local.rc
-echo 'on property:dev.bootcomplete=1' >> /system/etc/init.local.rc
-echo '        stop bootsound' >> /system/etc/init.local.rc
-echo '### Bootsound - Safe to Delete: End ###' >> /system/etc/init.local.rc
+bootsndcheck=$(grep -ico '###Bootsound' /system/etc/init.local.rc)
+if [ "$bootsndcheck" = "0"]; then
+	#This is to create the needed scripts in init.local.rc
+	echo '' >> /system/etc/init.local.rc
+	echo '' >> /system/etc/init.local.rc
+	echo '###Bootsound - Safe to Delete: Start ###' >> /system/etc/init.local.rc
+	echo "" >> /system/etc/init.local.rc
+	echo 'service bootsound /system/bin/bootsound' >> /system/etc/init.local.rc
+	echo '        user media' >> /system/etc/init.local.rc
+	echo '        group audio' >> /system/etc/init.local.rc
+	echo '        disabled' >> /system/etc/init.local.rc
+	echo '        oneshot' >> /system/etc/init.local.rc
+	echo '' >> /system/etc/init.local.rc
+	echo 'on property:init.svc.bootanim=running   # to correct timin' >> /system/etc/init.local.rc
+	echo '        start bootsound' >> /system/etc/init.local.rc
+	echo '' >> /system/etc/init.local.rc
+	echo 'on property:dev.bootcomplete=1' >> /system/etc/init.local.rc
+	echo '        stop bootsound' >> /system/etc/init.local.rc
+	echo '### Bootsound - Safe to Delete: End ###' >> /system/etc/init.local.rc
 
-#Now creating the bootsound script
-touch /system/bin/bootsound
-echo '#!/system/bin/sh' >> /system/bin/bootsound
-echo '' >> /system/bin/bootsound
-echo 'bprop=/system/build.prop' >> /system/bin/bootsound
-echo '' >> /system/bin/bootsound
-echo 'play=$(cat $bprop | egrep -c -i -o ro.config.play.bootsound=1)' >> /system/bin/bootsound
-echo 'if [ "$play" = "1" ]; then' >> /system/bin/bootsound
-echo '        stagefright -a -o /system/media/android_audio.mp3' >> /system/bin/bootsound
-echo 'fi' >> /system/bin/bootsound
-echo 'exit 0' >> /system/bin/bootsound
+	#Now creating the bootsound script
+	touch /system/bin/bootsound
+	echo '#!/system/bin/sh' >> /system/bin/bootsound
+	echo '' >> /system/bin/bootsound
+	echo 'bprop=/system/build.prop' >> /system/bin/bootsound
+	echo '' >> /system/bin/bootsound
+	echo 'play=$(cat $bprop | egrep -c -i -o ro.config.play.bootsound=1)' >> /system/bin/bootsound
+	echo 'if [ "$play" = "1" ]; then' >> /system/bin/bootsound
+	echo '        stagefright -a -o /system/media/android_audio.mp3' >> /system/bin/bootsound
+	echo 'fi' >> /system/bin/bootsound
+	echo 'exit 0' >> /system/bin/bootsound
 
-#Changing some permissions
-chmod 777 /system/bin/bootsound
-chown root:shell /system/bin/bootsound
-echo '' >> /system/build.pro
-echo 'ro.config.play.bootsound=1' >> /system/build.prop
+	#Changing some permissions
+	chmod 777 /system/bin/bootsound
+	chown root:shell /system/bin/bootsound
+	echo '' >> /system/build.pro
+	echo 'ro.config.play.bootsound=1' >> /system/build.prop
+else
+	echo "You already have boot sound configured"
+fi
 
+echo "Mounting system in ro mode"
 mount -o ro,remount /system
 exit
